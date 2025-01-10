dc190edc5b5d391f7ba83fb9aad96ce6dcca1b98
moonland <moonland.code@gmail.com>
2025-01-10 17:52:48 +0330

    Add files via upload

--- snake game.py
+++ snake game.py
@@ -1,42 +1,37 @@
-# Simple Snake Game in Python 3 for Beginners
-# By moonland
-
 import turtle
 import time
 import random
 
 delay = 0.1
-
-
 score = 0
 high_score = 0
-
 
 screen = turtle.Screen()
 screen.title("Snake Game by moonland v1")
 screen.bgcolor("khaki")
 screen.setup(width=600, height=600)
-screen.tracer(0) 
+screen.tracer(0)
 
+canvas = screen.getcanvas()
+root = canvas.winfo_toplevel()
+root.resizable(False, False)
 
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
 head.color("black")
 head.penup()
-head.goto(0,0)
+head.goto(0, 0)
 head.direction = "stop"
-
 
 food = turtle.Turtle()
 food.speed(0)
 food.shape("square")
 food.color("red")
 food.penup()
-food.goto(0,100)
+food.goto(0, 100)
 
 segments = []
-
 
 pen = turtle.Turtle()
 pen.speed(0)
@@ -45,8 +40,7 @@ pen.color("black")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("rubik", 24, "normal"))
-
+pen.write("Score: 0  High Score: 0", align="center", font=("Arial", 24, "normal"))
 
 def go_up():
     if head.direction != "down":
@@ -68,19 +62,15 @@ def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
-
     if head.direction == "down":
         y = head.ycor()
         head.sety(y - 20)
-
     if head.direction == "left":
         x = head.xcor()
         head.setx(x - 20)
-
     if head.direction == "right":
         x = head.xcor()
         head.setx(x + 20)
-
 
 screen.listen()
 screen.onkeypress(go_up, "Up")
@@ -88,40 +78,27 @@ screen.onkeypress(go_down, "Down")
 screen.onkeypress(go_left, "Left")
 screen.onkeypress(go_right, "Right")
 
-
 while True:
     screen.update()
 
-
-    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
         time.sleep(1)
-        head.goto(0,0)
+        head.goto(0, 0)
         head.direction = "stop"
-
 
         for segment in segments:
             segment.goto(1000, 1000)
-        
 
         segments.clear()
-
-
         score = 0
-
-
         delay = 0.1
-
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("rubik", 24, "normal")) 
-
-
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Arial", 24, "normal"))
 
     if head.distance(food) < 20:
-
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
-        food.goto(x,y)
-
+        food.goto(x, y)
 
         new_segment = turtle.Turtle()
         new_segment.speed(0)
@@ -130,56 +107,40 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
-
         delay -= 0.001
-
-
         score += 10
 
         if score > high_score:
             high_score = score
-        
+
         pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("rubik", 24, "normal")) 
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Arial", 24, "normal"))
 
-
-    for index in range(len(segments)-1, 0, -1):
-        x = segments[index-1].xcor()
-        y = segments[index-1].ycor()
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
         segments[index].goto(x, y)
-
 
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
-        segments[0].goto(x,y)
+        segments[0].goto(x, y)
 
-    move()    
-
+    move()
 
     for segment in segments:
         if segment.distance(head) < 20:
             time.sleep(1)
-            head.goto(0,0)
+            head.goto(0, 0)
             head.direction = "stop"
-        
 
             for segment in segments:
                 segment.goto(1000, 1000)
-        
 
             segments.clear()
-
-
             score = 0
-
-
             delay = 0.1
-        
-
             pen.clear()
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("rubik", 24, "normal"))
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Arial", 24, "normal"))
 
     time.sleep(delay)
-
-wn.mainloop()
